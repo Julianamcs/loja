e7119ac8645dcc7ffd1098d1b3a5e78600226b3a
Juliana Maciel <julyanamcs@gmail.com>
2023-07-19 07:52:12 -0300

    ajuste

--- src/br/com/alura/loja/orcamento/OrcamentoProxy.java
+++ src/br/com/alura/loja/orcamento/OrcamentoProxy.java
@@ -2,19 +2,17 @@ package br.com.alura.loja.orcamento;
 
 import java.math.BigDecimal;
 
-public class OrcamentoProxy extends Orcamento{
-	
-	private BigDecimal valor; 
+public class OrcamentoProxy extends Orcamento {
+
+	private BigDecimal valor;
 	private Orcamento orcamento;
-	
-	
+
 	public OrcamentoProxy(Orcamento orcamento) {
 		this.orcamento = orcamento;
 	}
-	
-	@Override
+
 	public BigDecimal getValor() {
-		if (valor == null) {
+		if (this.valor == null) {
 			this.valor = orcamento.getValor();
 		}
 		return this.valor;
